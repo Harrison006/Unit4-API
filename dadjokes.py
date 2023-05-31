df955fa07ddb7981c9c7b30d0d0f55766956585c
Harrison Bostock <87344112+Harrison006@users.noreply.github.com>
2023-05-31 16:17:22 +1000

    Update dadjokes.py

--- dadjokes.py
+++ dadjokes.py
@@ -2,7 +2,7 @@ import requests
 for _ in range(10):
     response = requests.get(
         "https://icanhazdadjoke.com/", 
-        headers={"Accept": "application/json"},
+        headers={"Accept": "application/json"}, # telling what output from https://icanhazdadjoke.com/ we want
     )
-    json_response = response.json()
-    print(json_response)
+    json_response = response.json()   # asking api for a JSON response
+    print(json_response) # printing response
